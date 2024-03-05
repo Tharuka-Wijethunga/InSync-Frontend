801eb6e16390d982429f9f7dc939d9f955c5a197
Tharuka Wijethunga <tharuka0621@gmail.com>
2024-03-05 23:37:37 +0530

    Add Records structure first steps.

--- App/Screens/AddRecords/AddRecords.jsx
+++ App/Screens/AddRecords/AddRecords.jsx
@@ -1,24 +1,23 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import React from 'react';
 import Colors from "../../Config/Colors";
-import {NativeBaseProvider, Button, View} from "native-base";
+import {NativeBaseProvider} from "native-base";
+import RecordForm from "./RecordForm";
 
 export default function AddRecords() {
     return (
         <NativeBaseProvider>
-            <View style={styles.container}>
-                <Text>AddRecords</Text>
-            </View>
+            <RecordForm/>
         </NativeBaseProvider>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         width: '100%',
         backgroundColor: Colors.BGColor,
         alignItems: 'center',
         flex: 1,
-        paddingTop: 50
+        paddingTop: 15
     }
 })
---
+++ App/Screens/AddRecords/RecordForm.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {Box, Button, HStack, IconButton, Input, NativeBaseProvider, Text, View, VStack} from "native-base";
+import Colors from "../../Config/Colors";
+import {FontAwesome6, MaterialIcons} from "@expo/vector-icons";
+import {StyleSheet} from "react-native";
+
+const RecordForm = () => {
+    return (
+        <NativeBaseProvider>
+            <View style={styles.container}>
+                <VStack space={4} w="94%" >
+                    <Box bg="white" rounded="2xl" shadow={3}>
+                        <VStack paddingX={4} paddingY={3} space={4}>
+                            <Box h="46px" bg={Colors.BGColor} rounded="9999"></Box>
+                            <VStack space={2}>
+                                <Text fontSize={20} fontWeight="medium">Amount</Text>
+                                <Input
+                                    variant="filled"
+                                    placeholder="-0"
+                                    InputLeftElement={
+                                        <Text fontWeight="bold" paddingLeft={4}>
+                                            LKR
+                                        </Text>
+                                    }
+                                    placeholderTextColor={Colors.Red}
+                                    bg={Colors.BGColor}
+                                    rounded="20"
+                                    h="80px"
+                                    fontSize={48}
+                                    textAlign="right"
+                                    caretHidden={true}
+                                    borderWidth={0}
+                                    keyboardType="numeric"
+                                    color={Colors.Red}
+                                />
+                            </VStack>
+                            <VStack>
+                                <Text fontSize={16} fontWeight="medium">Payment type</Text>
+                                <HStack space={20}>
+                                    <IconButton
+                                        icon={<FontAwesome6 name="credit-card" size={36} color={Colors.IconColor} />}
+                                        _pressed={{
+                                            _icon: {color: Colors.Blue}
+                                        }}
+                                        bgColor="transparent"
+                                    />
+                                    <IconButton
+                                        icon={<FontAwesome6 name="money-bill-1" size={38} color={Colors.IconColor}/>}
+                                        bgColor="transparent"
+                                        _pressed={{
+                                            _icon: {color: Colors.Blue}
+                                        }}
+                                    />
+                                </HStack>
+                            </VStack>
+                            <VStack>
+                                <Text fontSize={16} fontWeight="medium">Category</Text>
+                                <HStack>
+                                    <IconButton
+                                        icon={<MaterialIcons name="keyboard-arrow-right" size={36}
+                                                             color="black"/>}
+                                        // onPress={()=>}
+                                        borderRadius="full"
+                                        _pressed={{
+                                            bg: "blueGray.200:alpha.50"
+                                        }}
+                                    />
+                                </HStack>
+                            </VStack>
+                            <VStack>
+                                <Text fontSize={16} fontWeight="medium">Date & Time</Text>
+                            </VStack>
+                        </VStack>
+                    </Box>
+                    <Button bg={Colors.Blue} borderRadius={"full"} w={320} size="md" alignSelf="center">
+                        Save
+                    </Button>
+                </VStack>
+            </View>
+        </NativeBaseProvider>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: Colors.BGColor,
+        alignItems: 'center',
+        flex: 1,
+        paddingTop: 15
+    }
+})
+export default RecordForm;
