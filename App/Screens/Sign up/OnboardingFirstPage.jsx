0912201fe9ef589ed2c05d2008316278c9829d68
Tharuka Wijethunga <tharuka0621@gmail.com>
2024-03-14 01:54:42 +0530

    Onboarding First page done.

--- App/Screens/Sign up/OnboardingFirstPage.jsx
+++ App/Screens/Sign up/OnboardingFirstPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Dimensions } from 'react-native';
-import { NativeBaseProvider, Box, Input } from 'native-base';
+import {StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Dimensions} from 'react-native';
+import {NativeBaseProvider, View, Input, Text, Image, VStack} from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import Colors from '../../Config/Colors';
 import {useNavigation} from "@react-navigation/native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const OnboardingFirstPage = () => {
     const navigation = useNavigation();
@@ -30,40 +32,60 @@ const OnboardingFirstPage = () => {
     const handleInputBlur = () => {
         setIsInputFocused(false);
     };
-
+    const windowHeight = Dimensions.get('window').height;
     return (
         <NativeBaseProvider>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <Box style={styles.container}>
-                    <TouchableOpacity onPress={handleBack} style={styles.backButton}>
-                        <MaterialIcons name="keyboard-arrow-left" size={Dimensions.get('window').width * 0.06} color={Colors.black} />
-                        <Text fontWeight="bold" fontSize={Dimensions.get('window').width * 0.04} color={Colors.black} >Back</Text>
-                    </TouchableOpacity>
-                    <View style={styles.imageContainer}>
-                        <Image
-                            source={require("../../../assets/pic2.jpg")}
-                            style={styles.image}
-                            resizeMode="contain"
-                        />
-                    </View>
-                    <Text style={styles.title}>What's your Income range ?</Text>
-                    <View style={styles.inputContainer}>
-                        <Input
-                            variant="underlined"
-                            placeholder="LKR."
-                            value={incomeRange}
-                            onChangeText={text => {
-                                setIncomeRange(text);
-                            }}
-                            keyboardType="numeric" // Only allow numeric input
-                            onFocus={handleInputFocus}
-                            onBlur={handleInputBlur}
-                        />
-                    </View>
-                    <TouchableOpacity onPress={handleNext} style={styles.nextButton} >
-                        <MaterialIcons name="keyboard-arrow-right" size={Dimensions.get('window').width * 0.06} color={Colors.BGColor} />
-                    </TouchableOpacity>
-                </Box>
+                <SafeAreaView style={styles.container}>
+                        <TouchableOpacity onPress={handleBack} style={styles.backButton}>
+                            <MaterialIcons name="keyboard-arrow-left" size={24} color={Colors.black} />
+                            <Text fontWeight="bold" color={Colors.black} >Back</Text>
+                        </TouchableOpacity>
+                    <KeyboardAwareScrollView  resetScrollToCoords={{ x: 0, y: 0 }}
+                                              contentContainerStyle={{ flex: 1 }}
+                                              scrollEnabled={false}
+                                           style={{ flex: 1 }}>
+                            <View style={{ flex: 1 }}>
+                                <Image
+                                    source={require("../../../assets/pic2.jpg")}
+                                    style={styles.image}
+                                    alt='income'
+                                />
+
+                                <VStack space={3} paddingX={6} marginTop={windowHeight*0.60}>
+                                    <Text style={styles.title}>What's your Income range ?</Text>
+                                    <View marginBottom={10}>
+                                        <Input
+                                            w='80%'
+                                            variant="underlined"
+                                            InputLeftElement={
+                                                <Text fontWeight="medium" marginRight={4} color={Colors.IconColor}>
+                                                    LKR
+                                                </Text>
+                                            }
+                                            fontSize={20}
+                                            value={incomeRange}
+                                            onChangeText={text => {
+                                                setIncomeRange(text);
+                                            }}
+                                            keyboardType="numeric" // Only allow numeric input
+                                            onFocus={handleInputFocus}
+                                            onBlur={handleInputBlur}
+                                        />
+                                    </View>
+                                    <View paddingRight={2}>
+                                        <TouchableOpacity onPress={handleNext} style={styles.nextButton} >
+                                            <MaterialIcons name="keyboard-arrow-right" size={40} color='white' />
+                                        </TouchableOpacity>
+                                    </View>
+                                </VStack>
+                            </View>
+
+
+
+                    </KeyboardAwareScrollView>
+
+                </SafeAreaView>
             </TouchableWithoutFeedback>
         </NativeBaseProvider>
     );
@@ -72,45 +94,33 @@ const OnboardingFirstPage = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white'
     },
     backButton: {
         position: 'absolute',
-        top: Dimensions.get('window').height * 0.08,
-        left: Dimensions.get('window').width * 0.05,
+        top: '8%',
+        left: '5%',
         flexDirection: 'row',
         alignItems: 'center',
     },
     title: {
-        fontSize: Dimensions.get('window').width * 0.08,
+        fontSize: 28,
         fontWeight: 'bold',
-        marginBottom: Dimensions.get('window').height * 0.02,
-        marginTop: Dimensions.get('window').height * 0.1,
-    },
-    imageContainer: {
-        width: Dimensions.get('window').width * 0.9,
-        height: Dimensions.get('window').height * 0.3,
-        marginBottom: Dimensions.get('window').height * 0.04,
-        alignItems: 'center',
+        lineHeight: 30,
     },
     image: {
-        width: '100%',
-        height: '100%',
-    },
-    inputContainer: {
-        width: Dimensions.get('window').width * 0.8,
-        position: 'relative',
-    },
-    nextButton: {
+        width: '50%',
+        height: '22%',
         position: 'absolute',
-        bottom: Dimensions.get('window').height * 0.03,
-        right: Dimensions.get('window').width * 0.06,
+        top: '30%',
+        alignSelf: "center"
+    },
+
+    nextButton: {
         backgroundColor: Colors.Blue,
-        paddingVertical: Dimensions.get('window').height * 0.015,
-        paddingHorizontal: Dimensions.get('window').width * 0.05,
-        borderRadius: Dimensions.get('window').width * 0.05,
+        borderRadius: 20,
+        alignSelf: "flex-end"
     },
 });
 
