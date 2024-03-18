fc2aad17a8d3ed51d92df1dc28aae77590ae4fee
Lihaj <lihajwickramasinghe@gmail.com>
2024-03-19 01:04:22 +0530

    RecentActivities.android.jsx modal changed.

--- App/Screens/Dashboard/RecentActivities/RecentActivities.android.jsx
+++ App/Screens/Dashboard/RecentActivities/RecentActivities.android.jsx
@@ -7,25 +7,25 @@ import Records from "../Records";
 const RecentActivities = ({modalVisible, setModalVisible}) => {
     return (
         <Modal isOpen={modalVisible} animationPreset="slide">
-            <View backgroundColor={"white"}  marginTop={"30%"} marginBottom={"17%"}  w="94%" overflow={"hidden"} rounded="2xl" alignSelf="center" shadow={3}>
-                <VStack h="100%" w={"100%"} alignSelf="center" paddingTop={2}>
-                    <HStack justifyContent="space-between" alignItems="center" paddingLeft={2} >
-                            <IconButton
-                                icon={<MaterialIcons name="keyboard-arrow-left" size={28} color="black"/>}
-                                onPress={() => setModalVisible(!modalVisible)}
-                                borderRadius="full"
-                                _pressed={{
-                                    bg: "blueGray.200:alpha.50"
-                                }}
-                            />
-                            <Text fontSize={18} fontWeight={"medium"} paddingRight={4}>Recent Activities</Text>
-                        </HStack>
-                            <View h={0.480} bg="black"/>
-                    <VStack paddingX={4}>
-                        <Records/>
-                    </VStack>
-                </VStack>
-            </View>
+            {/*<View backgroundColor={"white"}  marginTop={"30%"} marginBottom={"17%"}  w="94%" overflow={"hidden"} rounded="2xl" alignSelf="center" shadow={3}>*/}
+                <Modal.Content  h="100%" bg="white" marginTop={"30%"} marginBottom={"17%"}  w="94%" rounded={"2xl"}>
+                <Modal.Header bg={"white"}>
+                    <HStack justifyContent="space-between" alignItems="center">
+                        <IconButton
+                            icon={<MaterialIcons name="keyboard-arrow-left" size={28} color="black"/>}
+                            onPress={() => setModalVisible(!modalVisible)}
+                            borderRadius="full"
+                            _pressed={{
+                                bg: "blueGray.200:alpha.50"
+                            }}
+                        />
+                        <Text fontSize={18} fontWeight={"medium"}>Recent Activities</Text>
+                    </HStack>
+                </Modal.Header>
+                <Modal.Body>
+                    <Records/>
+                </Modal.Body>
+            </Modal.Content>
         </Modal>
     );
 };
